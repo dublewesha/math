db55964fb5246595ee69631712954cce818bfc33
Даниил Петранцов <dubleweb@gmail.com>
2022-04-04 18:07:36 +0500

    Добавьте файлы проекта.

---
+++ math/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace math
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.robox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.alphabox = new System.Windows.Forms.TextBox();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(42, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите ро";
+            // 
+            // robox
+            // 
+            this.robox.Location = new System.Drawing.Point(35, 96);
+            this.robox.Name = "robox";
+            this.robox.Size = new System.Drawing.Size(135, 20);
+            this.robox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите альфа";
+            // 
+            // alphabox
+            // 
+            this.alphabox.Location = new System.Drawing.Point(35, 175);
+            this.alphabox.Name = "alphabox";
+            this.alphabox.Size = new System.Drawing.Size(135, 20);
+            this.alphabox.TabIndex = 4;
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.status.Location = new System.Drawing.Point(22, 363);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(164, 33);
+            this.status.TabIndex = 5;
+            this.status.Text = "Ожидание...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(198, 438);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.alphabox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.robox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "9";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox robox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox alphabox;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ math/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string s = robox.Text;
+                double roboxbox = Convert.ToDouble(s);
+                status.Text = "Выполнено";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка", "Ошибка: \n" + ex.Message);
+            }
+            //start();
+            status.Text = "Выполняю...";
+        }
+         /*public void start()
+        {
+            Form1 fact = new Form1();
+            int n = 2, m;
+            double trez, pk, alif, PO, sum, pup;
+            if (robox.Text)
+            double ro = System.Convert.ToDouble(robox.Text);
+            double ro = 2.17, eps = 0.08; //ro - ро, eps - альфа из задачи (эпсилон)
+            for (; n < 5; n++)
+            {
+                m = -1;
+                pk = 1;
+                sum = 1;
+                trez = ro / n;
+                double a = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    a = a * n / i * trez;
+                    sum += a;
+                }
+                double pks = 2;
+                //int test;
+                //test = fact.factorial(System.Convert.ToInt32(n));
+                //Console.WriteLine();
+                pup = Math.Pow(n, n) / fact.factorial(n) * Math.Pow(trez, n + 1) / (1 - trez);
+                alif = Math.Pow(n, n) / fact.factorial(n) * Math.Pow(trez, n - 1);
+                while (pk > eps && (Math.Abs(pk - pks) > 0.0000001))
+                {
+                    pks = pk;
+                    m++;
+                    PO = 1 / (sum + pup * (1 - Math.Pow(trez, m)));
+                    alif *= trez;
+                    pk = alif * PO;
+                }
+                if (Math.Abs(pk - pks) > 0.0000001)
+                {
+                    Console.WriteLine("n=" + n + " m=" + m);
+                }
+                else
+                    Console.WriteLine("n=" + n + " m=infinite");
+            }
+        }*/
+        public long factorial(long n)
+        {
+            long f = 1;
+            for (int i = 2; i <= n; i++)
+                f *= i;
+            return f;
+        }
+    }
+}
